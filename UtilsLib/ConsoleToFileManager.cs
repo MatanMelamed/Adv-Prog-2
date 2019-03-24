3c42c19404e16d904416a5456afceeb813c64bfc
Matan <melamed.matan1@gmail.com>
2019-03-24 14:45:33 +0200

    Utils

---
+++ UtilsLib/ConsoleToFileManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace UtilsLib {
+
+    public class ConsoleToFileManager {
+
+        FileStream fStream;
+        StreamWriter sWriter;
+        TextWriter tWriter;
+
+        string GetPathInProject(string fileName) {
+            string path = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString();
+            return path + "\\" + fileName;
+        }
+
+        public void StartWriter(string fileName = "consoleFile.txt") {
+            try {
+                fStream = File.Create(GetPathInProject(fileName));
+                sWriter = new StreamWriter(fStream);
+            }
+            catch (Exception e) {
+                Console.WriteLine("Cannot open Redirect.txt for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            tWriter = Console.Out;
+            Console.SetOut(sWriter);
+        }
+
+        public void EndWriter() {
+            Console.SetOut(tWriter);
+            sWriter.Dispose();
+            fStream.Dispose();
+            Console.WriteLine("ConsoleToFileManager has finished.");
+        }
+    }
+
+    public class tester {
+        public void printTest() {
+            Console.WriteLine("tester test print called.");
+        }
+
+        public int getTest() {
+            return 100;
+        }
+    }
+}
---
+++ UtilsTester/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsTester {
+    class Program {
+        static void Main(string[] args) {
+            UtilsLib.ConsoleToFileManager cm = new UtilsLib.ConsoleToFileManager();
+            cm.StartWriter("testfile.txt");
+            Console.WriteLine("try");
+            cm.EndWriter();
+        }
+    }
+}
